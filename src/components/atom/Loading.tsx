fa9d918b56067726e3c6e2b2be88eaa776cb1384
Joseph Park <rkfnahs12e@gmail.com>
2024-09-11 20:03:12 +0900

    feat : loading 컴포넌트 적용

--- src/components/atom/Loading.tsx
+++ src/components/atom/Loading.tsx
@@ -2,13 +2,15 @@ import { RotatingLines } from "react-loader-spinner";
 
 function Loading() {
   return (
-    <RotatingLines
-      strokeColor="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      width="96"
-      visible={true}
-    />
+    <div className="flex justify-center items-center h-screen">
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
   );
 }
 
